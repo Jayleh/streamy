82b329281c654eebc9a3158d1466f3495206aa5a
Jayleh <jaylehyun@gmail.com>
2018-12-11 13:34:34 -0800

    integrating redux, hooking up StreamCreate

--- client/src/componenets/streams/StreamCreate.js
+++ client/src/componenets/streams/StreamCreate.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends Component {
+  renderInput({ input }) {
+    // ... taking all props and putting it onto the input
+    // onChange, value props, etc.
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name='title' component={this.renderInput} />
+        <Field name='description' component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+// reduxForm is similar to the connect function
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
