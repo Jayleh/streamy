de71c92d994f149a8509bbbc1a55423b0702758c
Jayleh <jaylehyun@gmail.com>
2018-12-12 16:52:44 -0800

    hooked up createStream, starting StreamEdit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -7,7 +9,6 @@ import {
   EDIT_STREAM,
   DELETE_STREAM
 } from './types';
-import streams from '../apis/streams';
 
 export const signIn = userId => {
   return {
@@ -22,10 +23,15 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  // Do some programmatic navigation to
+  // get the user back to the root route
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
